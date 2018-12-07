331ef525077486040c0352eb3e74bc4c439b81c6
smondal <mondalda@gmail.com>
2018-12-06 17:40:34 -0800

    used vector to store the child node

--- trie.cpp
+++ trie.cpp
@@ -1,37 +1,69 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <array>
 
 using namespace std;
 
 struct node {
 	char c;
 	node * parent;
-	unordered_map <char, node*> childs;
-	int word_number;
+	array <node*, 26> childs;
+	vector<int> word_occerence;
 };
 
 
-void insert_word (node * head, string word){
+void insert_word (node * head, string word, int index){
 
 	node * traverse = head;
 	for (char& c : word ){
-		node * temp = new node();
-        	temp->c = c;
-        	temp->parent = traverse;
-        	pair<char, node*> children (c,temp);
-        	traverse->childs.insert(children);
-		traverse = temp;
+		
+		if (traverse->childs[c-'a'] == NULL){
+			node * cnode = new node();
+        		cnode->c = c;
+			cnode->parent = traverse;
+        		traverse->childs[c-'a'] = cnode;
+			traverse = cnode; 
+		}else{
+			traverse = traverse->childs[c-'a'];
+		}
+
+		if(word.back() == c){
+			traverse->word_occerence.push_back(index);
+		}
+
 	}
 
-	/*
-	if(traverse->childs.size() == 0){
-		cout << "No child exist \n";
-	}else{	
-		cout << "child created" << traverse->childs.size();
-	}*/	
-	
 }
+
+
+
+
+bool search_tries(node * head, string word){
+
+	node * traverse = head;
+
+	for(char &c : word){
+
+		cout<< traverse->c << "\n";
+
+		if((word.back() == c) && (traverse->word_occerence.size() == 0))
+			return true;
+
+		if((word.back() != c) && traverse->childs[c-'a'] == NULL){
+
+			cout << "got null pointer \n";
+			return false;
+		}
+		else{
+			traverse = traverse->childs[c-'a'];
+		}	
+	}
+	
+	return false;
+}
+
+
 
 int main ()
 {
@@ -39,20 +71,51 @@ int main ()
 	
 	node * head = new node();
 	head->c = '/'; // root char
-        head->word_number = 0; 
 
-	string word = "test";
-	insert_word(head, word);
+	string word = "tesp";
+		
+	insert_word(head, word, 1);
+	insert_word(head, "temp", 2);
 
-	while(true){
-		cout << head->c;
-		if(head->childs.size() == 0){	
-			cout << "Foun the word\n";
-			break;
-		}
-		auto it = head->childs.begin();
-		head = it->second;
+
+	bool r = search_tries(head,"tespu");
+
+
+	if(r){
+
+		cout << "Found";
+	}
+	else{
+	
+		cout << "Not Found";
 	}
 
+	/*while(true){
+
+		cout << head->c;
+
+		bool is_child_exist = false;
+		node * next = new node();
+
+		for(int i = 0 ; i < 26 ; i++){
+			
+			if(head->childs[i] != NULL){	
+				is_child_exist = true;
+				next = head->childs[i];
+				break;
+			}
+		}
+
+		if (!is_child_exist){
+			cout << "Found a word \n";
+			break;
+		
+		}
+		
+		head = next;
+
+	}*/
+
+	
 	return 0;
 }
