3e8f604031b9e44763117e2de2aae1de337f0f9a
sukanto <mondalda@gmail.com>
2019-02-01 00:06:11 -0800

    delete node form a linked list

---
+++ linkedlist/delete_node_without_head_pointer.cpp
@@ -0,0 +1,19 @@
+/* Author : Sukanto Mondal */
+
+
+/*Delete a Node from linked list without head pointer
+ *
+ * You are given a singly linked list and pointer which is pointing to the node which is required to be deleted. Any information about head pointer or any other node is not given. You need to write a function to delete that node from linked list. Your function will take only one argument: pointer to the node which is to be deleted.
+ *
+ */
+#include <iostrem>
+
+using namespace std;
+
+void delete_node(){
+}
+
+int main(){
+
+	return 0;
+}
